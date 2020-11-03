ca3c5f1ced4cd8b4e92c30b5562beb7a2fd944dd
Jordy Pena <jordypenamontes@gmail.com>
2020-11-03 14:09:56 -0600

    added post and delete refactoring

--- src/app.js
+++ src/app.js
@@ -1,156 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
-const { v4: uuid} = require("uuid")
-const winston = require('winston')
+  
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
+const app = express()
 
-const app = express();
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
 
-const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+app.use(bookmarksRouter)
 
-const bookmarks = [
-  { id: uuid(),
-    title: 'Thinkful',
-    url: 'https://www.thinkful.com',
-    description: 'Think outside the classroom',
-    rating: 5 },
-  { id: uuid(),
-    title: 'Google',
-    url: 'https://www.google.com',
-    description: 'Where we find everything else',
-    rating: 4 },
-  { id: uuid(),
-    title: 'MDN',
-    url: 'https://developer.mozilla.org',
-    description: 'The only place to find web documentation',
-    rating: 5 },
-]
-
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
-app.use(express.json())
-
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`)
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware
-  next()
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler)
 
-
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-app.get('/bookmarks', (req, res) => {
-  res.send(bookmarks)
-})
-
-app.get('/bookmarks/:id', (req, res) => {
-  console.log(req.params)
-  const {id} = req.params
-  const bookmark = bookmarks.find(c => c.id == id )
-
-  if(!bookmark) {
-    logger.error(`Bookmark with id ${id} not found`)
-    return res
-      .status(404)
-      .send('Bookmark Not Found')
-  }
-  res.json(bookmark)
-})
-
-app.post('/bookmarks', (req, res) => {
-  const {title, description} = req.body
-
-  if(!title) {
-    logger.error('Title is required')
-    return res
-      .status(400)
-      .send('Invalid data')
-  }
-
-  if(!description) {
-    logger.error('Description is required')
-    return res
-      .status(400)
-      .send('Invalid data')
-  }
-
-  const id = uuid()
-
-  const bookmark = {
-    id,
-    title,
-    description
-  }
-  bookmarks.push(bookmark)
-
-  logger.info(`Card with id ${id} created`)
-
-  res
-    .status(201)
-    .location(`https://localhost:8000/card${id}`)
-    .json(bookmarks)
-})
-
-app.delete('/bookmarks/:id', (req,res) => {
-  const {id} = req.params
-
-  const bookmarkIndex = bookmarks.findIndex(b => b.id == id)
-
-  if(bookmarkIndex === -1) {
-    logger.error(`Bookmark with id ${id} not found`)
-    return res
-      .status(404)
-      .send('Not Found')
-  }
-
-  bookmarks.splice(bookmarkIndex, 1)
-
-  logger.info(`Bookmark with id ${id} deleted.`)
-  res
-    .status(204)
-    .end()
-})
-
-
-
-// set up winston
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
-
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
-
-module.exports = app;
+module.exports = app
